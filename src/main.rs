af9774fadbc624557414af9369367c46a169b804
Wilper-st <keik.nd000@gmail.com>
2022-06-19 23:36:03 +0300

    Added player inertia and blade throwing

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
+use std::array;
 use std::f32::consts::PI;
+use std::os::windows::prelude;
 
-use std::{
+/*use std::{
     cell::{Ref, RefCell},
     rc::Rc
-};
+};*/
 
 use rand::Rng;
 
-use glam::{DVec2, Vec2};
+use glam::{DVec2, /*Vec2*/};
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::{Window, Event, ButtonEvent, ButtonArgs, Key, ButtonState};
+use piston::{Window, /*Event,*/ ButtonEvent, ButtonArgs, Key, ButtonState, MouseButton, EventLoop, PressEvent, MouseCursorEvent, CursorEvent};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent, Button};
 use piston::window::WindowSettings;
@@ -36,7 +38,7 @@ impl Defender {
     pub fn move_to_player(&mut self, player_x: f64, player_y: f64, args: &UpdateArgs){
 
         let p_pos = DVec2::new(player_x, player_y);
-        let mut pos = DVec2::new(self.position.x, self.position.y);
+        let pos = DVec2::new(self.position.x, self.position.y);
         
         let direction = p_pos - pos;
         let angle = f64::atan2(direction.y, direction.x) * 180.0/f64::from(PI);
@@ -44,6 +46,12 @@ impl Defender {
         self.vec_speed = direction.normalize() * self.speed * args.dt;
         let res_pos = pos + self.vec_speed;
         self.position = res_pos;
+    }
+}
+
+impl Drop for Defender{
+    fn drop(&mut self) {
+        println!("Deffender is dropped!");
     }
 }
 
@@ -61,14 +69,45 @@ fn create_defenders(count: u32) -> Vec<Defender> {
     }).collect()
 }
 
+pub struct Blade{
+    x: f64,
+    y:f64,
+    size: f64,
+    speed: f64,
+    rotation: f64,
+    direction: DVec2,
+}
+
+impl Blade {
+    fn move_forward(&mut self, args: &UpdateArgs){
+        let pos = DVec2::new(self.x, self.y);
+        let vec_speed = self.direction * self.speed * args.dt;
+        let res_pos = pos + vec_speed;
+        self.x = res_pos[0];
+        self.y = res_pos[1];
+        self.speed += 3.0 * self.speed * args.dt;
+    }
+}
+
+impl Drop for Blade {
+    fn drop(&mut self) {
+        println!("Blade is dropped!");
+    }
+}
+
 pub struct Game {
     gl: GlGraphics, // OpenGL drawing backend.
     is_over: bool,
     defenders: Vec<Defender>,
+    blades: Vec<Blade>,
+    cursor_pos: [f64; 2],
     player_x: f64, player_y: f64,
     size: f64,
     rotation: f64,  // Rotation for the player.
-    speed: f64,
+    axel: f64,
+    speed_max_mod: f64,
+    speed_x: f64,
+    speed_y: f64,
     rotation_speed: f64,
     m_right: bool,
     m_left: bool,
@@ -101,6 +140,16 @@ impl Game {
                 .trans(-i.size/2.0, -i.size/2.0);
 
             rectangle(RED, rectangle::square(0.0, 0.0, i.size), transform, gl);
+            }
+
+            for i in &self.blades{
+                let transform = c
+                .transform
+                .trans(i.x, i.y)
+                .rot_deg(i.rotation)
+                .trans(-i.size/2.0, -i.size/2.0);
+
+            rectangle(RED, rectangle::square(0.0, 0.0, i.size), transform, gl); 
             }
 
             rectangle(RED, square, transform, gl);
@@ -139,29 +188,56 @@ impl Game {
                 ButtonState::Release => self.m_right = false
             }
         }
+
+        if args.button == Button::Mouse(MouseButton::Left) && args.state == ButtonState::Release{//create new blade
+            let p_pos = DVec2::new(self.player_x, self.player_y);
+            let pos = DVec2::new(self.cursor_pos[0], self.cursor_pos[1]);
+        
+            let direction = pos - p_pos;
+            let angle = f64::atan2(direction.y, direction.x) * 180.0/f64::from(PI);
+            self.blades.push(Blade{ x: self.player_x, y: self.player_y, size: 30.0, speed: 300.0, rotation: angle, direction: direction.normalize() });
+        }
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 5 radians per second.
 
         self.rotation += self.rotation_speed * args.dt;
+
         if !self.is_over{
-            if self.m_left == true {
-                self.player_x -= self.speed * args.dt;
+            if self.m_left {
+                self.speed_x -= self.axel * args.dt;
+                if self.speed_x <= -self.speed_max_mod { self.speed_x = -self.speed_max_mod; }
             }
 
-            if self.m_up == true {
-               self.player_y -= self.speed * args.dt;
+            if self.m_up {
+                self.speed_y -= self.axel * args.dt;
+                if self.speed_y <= -self.speed_max_mod { self.speed_y = -self.speed_max_mod; }
             }
 
-            if self.m_down == true {
-                self.player_y += self.speed * args.dt;
+            if self.m_down {
+                self.speed_y += self.axel * args.dt;
+                if self.speed_y >= self.speed_max_mod { self.speed_y = self.speed_max_mod; }
             }
 
-            if self.m_right == true {
-                self.player_x += self.speed * args.dt;
+            if self.m_right {
+                self.speed_x += self.axel * args.dt;
+                if self.speed_x >= self.speed_max_mod { self.speed_x = self.speed_max_mod; }
             }
-        }else{
+
+            if !self.m_right && !self.m_left{
+                if self.speed_x > 0.0 { self.speed_x -= self.axel * args.dt; }
+                if self.speed_x < 0.0 { self.speed_x += self.axel * args.dt; }
+            }
+
+            if !self.m_up && !self.m_down{
+                if self.speed_y > 0.0 { self.speed_y -= self.axel * args.dt; }
+                if self.speed_y < 0.0 { self.speed_y += self.axel * args.dt; }
+            }
+
+            self.player_x += self.speed_x * args.dt;
+            self.player_y += self.speed_y * args.dt;
+        }
+        else{
             self.rotation_speed -= self.rotation_speed/1.1 * args.dt;
             if self.rotation_speed <= 0.0 { self.rotation_speed = 0.0; }
         }
@@ -174,6 +250,10 @@ impl Game {
             }
 
             i.move_to_player(self.player_x, self.player_y, &args);
+        }
+
+        for i in &mut self.blades {
+            i.move_forward(&args);
         }
     }
 }
@@ -191,20 +271,27 @@ fn main() {
         .unwrap();
 
     // Fill in defenders list
-    let defenders:Vec<Defender> = create_defenders(10); // list of all creatures
+    let defenders: Vec<Defender> = create_defenders(10); // list of all creatures
 
     //defenders.into_iter().map(|x|, if);
+
+    let blades: Vec<Blade> = vec![];
 
     // Create a new game and run it.
     let mut game = Game {
         gl: GlGraphics::new(opengl),
         is_over: false,
         defenders: defenders,
+        blades: blades,
+        cursor_pos: [0.0, 0.0],
         player_x:window.size().width/2.0,// player`s start position
         player_y:window.size().height/2.0,//
         size:40.0,
         rotation: 0.0,
-        speed: 300.0,
+        axel: 2300.0,
+        speed_max_mod: 300.0,
+        speed_x: 0.0,
+        speed_y: 0.0,
         rotation_speed: 8.0,
         m_right: false,
         m_left: false,
@@ -212,8 +299,12 @@ fn main() {
         m_down: false
     };
 
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new().lazy(false));
     while let Some(e) = events.next(&mut window) {
+        
+        if let Some(args) = e.mouse_cursor_args() {
+            game.cursor_pos = args;
+        }
 
         if let Some(args) = e.render_args() {
             game.render(&args);
