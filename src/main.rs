c977acef5c448bfc0319e9b015d36b12600622ba
Wilper-st <keik.nd000@gmail.com>
2022-11-15 22:59:09 +0300

    Restart function on a R button added; Creatin of an enemy on a RMB button added; Ability to change the size of the created enemies on Up and Down buttons.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
-use std::array;
 use std::f32::consts::PI;
-use std::os::windows::prelude;
 
 /*use std::{
     cell::{Ref, RefCell},
@@ -14,34 +12,40 @@ use std::os::windows::prelude;
 
 use rand::Rng;
 
-use glam::{DVec2, /*Vec2*/};
+use glam::DVec2;
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::{Window, /*Event,*/ ButtonEvent, ButtonArgs, Key, ButtonState, MouseButton, EventLoop, PressEvent, MouseCursorEvent, CursorEvent};
 use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent, Button};
+use piston::input::{Button, RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
+use piston::{
+    ButtonArgs, /*Event,*/ ButtonEvent, ButtonState, EventLoop, Key, MouseButton,
+    MouseCursorEvent, Window,
+};
 
 const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
 const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
+#[derive(Clone, Debug)]
 pub struct Defender {
-    id: u32,
     position: DVec2,
     size: f64,
     rotation: f64,
     speed: f64,
-    vec_speed: DVec2
+    vec_speed: DVec2,
 }
 
 impl Defender {
-    pub fn move_to_player(&mut self, player_x: f64, player_y: f64, args: &UpdateArgs){
+    pub fn collide(&self, _colliding_blade: &Blade) {}
 
+    pub fn evolve() {}
+
+    pub fn move_to_player(&mut self, player_x: f64, player_y: f64, args: &UpdateArgs) {
         let p_pos = DVec2::new(player_x, player_y);
         let pos = DVec2::new(self.position.x, self.position.y);
-        
+
         let direction = p_pos - pos;
-        let angle = f64::atan2(direction.y, direction.x) * 180.0/f64::from(PI);
+        let angle = f64::atan2(direction.y, direction.x) * 180.0 / f64::from(PI);
         self.rotation = angle;
         self.vec_speed = direction.normalize() * self.speed * args.dt;
         let res_pos = pos + self.vec_speed;
@@ -49,29 +53,29 @@ impl Defender {
     }
 }
 
-impl Drop for Defender{
+impl Drop for Defender {
     fn drop(&mut self) {
-        println!("Deffender is dropped!");
+        println!("Defender is dropped!");
     }
 }
 
 fn create_defenders(count: u32) -> Vec<Defender> {
-
     let mut rng = rand::thread_rng();
 
-    (0..count).map( |x| Defender {
-        id: x,
-        position: DVec2::new(rng.gen_range(0..800) as f64, rng.gen_range(0..600) as f64),
-        size: 15.0,
-        rotation: 0.0,
-        speed: 80.0,
-        vec_speed: DVec2::new(0.0, 0.0)
-    }).collect()
+    (0..count)
+        .map(|_| Defender {
+            position: DVec2::new(rng.gen_range(0..800) as f64, rng.gen_range(0..600) as f64),
+            size: 15.0,
+            rotation: 0.0,
+            speed: 80.0,
+            vec_speed: DVec2::new(0.0, 0.0),
+        })
+        .collect()
 }
 
-pub struct Blade{
+pub struct Blade {
     x: f64,
-    y:f64,
+    y: f64,
     size: f64,
     speed: f64,
     rotation: f64,
@@ -79,7 +83,7 @@ pub struct Blade{
 }
 
 impl Blade {
-    fn move_forward(&mut self, args: &UpdateArgs){
+    fn move_forward(&mut self, args: &UpdateArgs) {
         let pos = DVec2::new(self.x, self.y);
         let vec_speed = self.direction * self.speed * args.dt;
         let res_pos = pos + vec_speed;
@@ -98,12 +102,14 @@ impl Drop for Blade {
 pub struct Game {
     gl: GlGraphics, // OpenGL drawing backend.
     is_over: bool,
+    restart: bool,
     defenders: Vec<Defender>,
-    blades: Vec<Blade>,
+    blades: Vec<Blade>, // All blades in the game
     cursor_pos: [f64; 2],
-    player_x: f64, player_y: f64,
+    player_x: f64,
+    player_y: f64,
     size: f64,
-    rotation: f64,  // Rotation for the player.
+    rotation: f64, // Rotation for the player.
     axel: f64,
     speed_max_mod: f64,
     speed_x: f64,
@@ -112,7 +118,8 @@ pub struct Game {
     m_right: bool,
     m_left: bool,
     m_up: bool,
-    m_down: bool
+    m_down: bool,
+    defender_create_size: f64,
 }
 
 impl Game {
@@ -130,130 +137,208 @@ impl Game {
                 .transform
                 .trans(self.player_x, self.player_y)
                 .rot_rad(rotation)
-                .trans(-self.size/2.0, -self.size/2.0);
+                .trans(-self.size / 2.0, -self.size / 2.0);
 
             for i in &self.defenders {
                 let transform = c
-                .transform
-                .trans(i.position.x, i.position.y)
-                .rot_deg(i.rotation)
-                .trans(-i.size/2.0, -i.size/2.0);
+                    .transform
+                    .trans(i.position.x, i.position.y)
+                    .rot_deg(i.rotation)
+                    .trans(-i.size / 2.0, -i.size / 2.0);
 
-            rectangle(RED, rectangle::square(0.0, 0.0, i.size), transform, gl);
+                rectangle(RED, rectangle::square(0.0, 0.0, i.size), transform, gl);
             }
 
-            for i in &self.blades{
+            for i in &self.blades {
                 let transform = c
-                .transform
-                .trans(i.x, i.y)
-                .rot_deg(i.rotation)
-                .trans(-i.size/2.0, -i.size/2.0);
+                    .transform
+                    .trans(i.x, i.y)
+                    .rot_deg(i.rotation)
+                    .trans(-i.size / 2.0, -i.size / 2.0);
 
-            rectangle(RED, rectangle::square(0.0, 0.0, i.size), transform, gl); 
+                rectangle(RED, rectangle::square(0.0, 0.0, i.size), transform, gl);
             }
 
             rectangle(RED, square, transform, gl);
         });
     }
 
-    fn controll(&mut self, args: &ButtonArgs){
-        if args.button == Button::Keyboard(Key::A) 
-        {
+    fn controll(&mut self, args: &ButtonArgs) {
+        if args.button == Button::Keyboard(Key::A) {
             match args.state {
                 ButtonState::Press => self.m_left = true,
-                ButtonState::Release => self.m_left = false
+                ButtonState::Release => self.m_left = false,
             }
         }
 
-        if args.button == Button::Keyboard(Key::W) 
-        {
+        if args.button == Button::Keyboard(Key::W) {
             match args.state {
                 ButtonState::Press => self.m_up = true,
-                ButtonState::Release => self.m_up = false
+                ButtonState::Release => self.m_up = false,
             }
         }
 
-        if args.button == Button::Keyboard(Key::S) 
-        {
+        if args.button == Button::Keyboard(Key::S) {
             match args.state {
                 ButtonState::Press => self.m_down = true,
-                ButtonState::Release => self.m_down = false
+                ButtonState::Release => self.m_down = false,
             }
         }
 
-        if args.button == Button::Keyboard(Key::D) 
-        {
+        if args.button == Button::Keyboard(Key::D) {
             match args.state {
                 ButtonState::Press => self.m_right = true,
-                ButtonState::Release => self.m_right = false
+                ButtonState::Release => self.m_right = false,
             }
         }
 
-        if args.button == Button::Mouse(MouseButton::Left) && args.state == ButtonState::Release{//create new blade
+        if args.button == Button::Keyboard(Key::R) && args.state == ButtonState::Release {
+            self.restart = true;
+        }
+
+        if args.button == Button::Mouse(MouseButton::Left) && args.state == ButtonState::Release {
+            //create new blade
+            println!("LBM is pressed!");
             let p_pos = DVec2::new(self.player_x, self.player_y);
             let pos = DVec2::new(self.cursor_pos[0], self.cursor_pos[1]);
-        
+
             let direction = pos - p_pos;
-            let angle = f64::atan2(direction.y, direction.x) * 180.0/f64::from(PI);
-            self.blades.push(Blade{ x: self.player_x, y: self.player_y, size: 30.0, speed: 300.0, rotation: angle, direction: direction.normalize() });
+            let angle = f64::atan2(direction.y, direction.x) * 180.0 / f64::from(PI);
+            self.blades.push(Blade {
+                x: self.player_x,
+                y: self.player_y,
+                size: 10.0,
+                speed: 300.0,
+                rotation: angle,
+                direction: direction.normalize(),
+            });
+        }
+
+        if args.button == Button::Mouse(MouseButton::Right) && args.state == ButtonState::Release {
+            println!("RBM is pressed!");
+            let pos = DVec2::new(self.cursor_pos[0], self.cursor_pos[1]);
+
+            self.defenders.push(Defender {
+                position: pos,
+                size: self.defender_create_size,
+                rotation: 0.0,
+                speed: 80.0 * 15.0 / self.defender_create_size,
+                vec_speed: DVec2::new(0.0, 0.0),
+            });
+        }
+
+        if args.button == Button::Keyboard(Key::Up) && args.state == ButtonState::Release {
+            self.defender_create_size += 1.0;
+        }
+        if args.button == Button::Keyboard(Key::Down) && args.state == ButtonState::Release {
+            self.defender_create_size -= 1.0;
         }
     }
 
     fn update(&mut self, args: &UpdateArgs) {
+        //let mut new_blades: Vec<Blade> = vec![];
 
         self.rotation += self.rotation_speed * args.dt;
 
-        if !self.is_over{
+        if self.restart {
+            self.defenders.clear();
+            self.blades.clear();
+
+            if self.is_over {
+                self.is_over = false;
+            }
+
+            self.rotation_speed = 8.0;
+
+            self.player_x = 800.0 / 2.0;
+            self.player_y = 600.0 / 2.0;
+
+            self.restart = false;
+        }
+
+        if !self.is_over {
+            // movement after controlls detection
             if self.m_left {
                 self.speed_x -= self.axel * args.dt;
-                if self.speed_x <= -self.speed_max_mod { self.speed_x = -self.speed_max_mod; }
+                if self.speed_x <= -self.speed_max_mod {
+                    self.speed_x = -self.speed_max_mod;
+                }
             }
 
             if self.m_up {
                 self.speed_y -= self.axel * args.dt;
-                if self.speed_y <= -self.speed_max_mod { self.speed_y = -self.speed_max_mod; }
+                if self.speed_y <= -self.speed_max_mod {
+                    self.speed_y = -self.speed_max_mod;
+                }
             }
 
             if self.m_down {
                 self.speed_y += self.axel * args.dt;
-                if self.speed_y >= self.speed_max_mod { self.speed_y = self.speed_max_mod; }
+                if self.speed_y >= self.speed_max_mod {
+                    self.speed_y = self.speed_max_mod;
+                }
             }
 
             if self.m_right {
                 self.speed_x += self.axel * args.dt;
-                if self.speed_x >= self.speed_max_mod { self.speed_x = self.speed_max_mod; }
+                if self.speed_x >= self.speed_max_mod {
+                    self.speed_x = self.speed_max_mod;
+                }
             }
 
-            if !self.m_right && !self.m_left{
-                if self.speed_x > 0.0 { self.speed_x -= self.axel * args.dt; }
-                if self.speed_x < 0.0 { self.speed_x += self.axel * args.dt; }
+            if !self.m_right && !self.m_left {
+                if self.speed_x > 0.0 {
+                    self.speed_x -= self.axel * args.dt;
+                }
+                if self.speed_x < 0.0 {
+                    self.speed_x += self.axel * args.dt;
+                }
             }
 
-            if !self.m_up && !self.m_down{
-                if self.speed_y > 0.0 { self.speed_y -= self.axel * args.dt; }
-                if self.speed_y < 0.0 { self.speed_y += self.axel * args.dt; }
+            if !self.m_up && !self.m_down {
+                if self.speed_y > 0.0 {
+                    self.speed_y -= self.axel * args.dt;
+                }
+                if self.speed_y < 0.0 {
+                    self.speed_y += self.axel * args.dt;
+                }
             }
 
             self.player_x += self.speed_x * args.dt;
             self.player_y += self.speed_y * args.dt;
-        }
-        else{
-            self.rotation_speed -= self.rotation_speed/1.1 * args.dt;
-            if self.rotation_speed <= 0.0 { self.rotation_speed = 0.0; }
+        } else {
+            // die motion
+            self.rotation_speed -= self.rotation_speed / 1.1 * args.dt;
+            if self.rotation_speed <= 0.0 {
+                self.rotation_speed = 0.0;
+            }
         }
 
         for i in &mut self.defenders {
-
             let pdistance = i.position - DVec2::new(self.player_x, self.player_y);
-            if pdistance.length() < (self.size/2.0 + i.size/2.0) {
+            if pdistance.length() < (self.size / 2.0 + i.size / 2.0) {
                 self.is_over = true;
             }
 
             i.move_to_player(self.player_x, self.player_y, &args);
         }
 
+        let mut extra_blades_index: Vec<i32> = vec![]; // All extra blades` indexes
+
+        let mut p = -1; // This is an index of an extra blade inside an array. -1 bc arrays starts from 0
+
         for i in &mut self.blades {
             i.move_forward(&args);
+            p += 1;
+
+            let pdistance = DVec2::new(i.x, i.y) - DVec2::new(self.player_x, self.player_y);
+            if pdistance.length() > 5000.0 {
+                extra_blades_index.push(p);
+            }
+        }
+
+        for i in extra_blades_index {
+            self.blades.remove(i.try_into().unwrap());
         }
     }
 }
@@ -281,12 +366,13 @@ fn main() {
     let mut game = Game {
         gl: GlGraphics::new(opengl),
         is_over: false,
+        restart: false,
         defenders: defenders,
         blades: blades,
         cursor_pos: [0.0, 0.0],
-        player_x:window.size().width/2.0,// player`s start position
-        player_y:window.size().height/2.0,//
-        size:40.0,
+        player_x: window.size().width / 2.0, // player`s start position
+        player_y: window.size().height / 2.0, //
+        size: 40.0,
         rotation: 0.0,
         axel: 2300.0,
         speed_max_mod: 300.0,
@@ -296,12 +382,12 @@ fn main() {
         m_right: false,
         m_left: false,
         m_up: false,
-        m_down: false
+        m_down: false,
+        defender_create_size: 15.0,
     };
 
     let mut events = Events::new(EventSettings::new().lazy(false));
     while let Some(e) = events.next(&mut window) {
-        
         if let Some(args) = e.mouse_cursor_args() {
             game.cursor_pos = args;
         }
@@ -311,7 +397,7 @@ fn main() {
         }
 
         if let Some(args) = e.button_args() {
-            game.controll(&args);// player controls
+            game.controll(&args); // player controls
         }
 
         if let Some(args) = e.update_args() {
